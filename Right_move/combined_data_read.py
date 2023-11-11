feaa9078117a25472490ff5567ae789c816cb573
jontysh4w <79010302+jontysh4w@users.noreply.github.com>
2023-11-11 11:46:16 +0000

    Output dictionary of class values to json file

--- Right_move/combined_data_read.py
+++ Right_move/combined_data_read.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import re
+import json 
 rent_stats= 'Right_move/londonrentalstatisticsq22023.xlsx'
 dat1 = pd.read_excel(rent_stats,sheet_name="Table 1.3", header=12,nrows=1974)
 #print(dat1)
@@ -19,6 +21,17 @@ i1=0
 i2=0
 
 
+class Property:
+    def __init__(self, link, location, price, mean_price, distance):
+        self.link= link
+        self.location= location
+        self.price = price
+        self.mean_price = mean_price
+        self.distance = distance
+    def __str__(self):
+        return f"Link: {self.link}, Location: {self.location}, Price: {self.price}, Mean price:{self.mean_price}, Distance: {self.distance}"
+
+property_list=[]
 # data structure link, location , house , price
 for i in data:
     x=str(i[1])
@@ -30,13 +43,15 @@ for i in data:
     if len(y)<=3:
         i1+=1
         for row in data_new:
-            if row[2]=='Three Bedrooms' and type(row[4])==int and row[1]==y :
+            if row[2]=='Three Bedrooms' and type(row[4])==int and (row[1]==y or row[1]==y2) :
                 i2+=1
-                Property_score.append()
-            if row[2]=='Three Bedrooms' and type(row[4])==int and row[1]==y2:
-                i2+=1
-
+                #price=re.findall('[0-9]+',i[3].replace(",",""))
+                price_2=int(''.join(filter(str.isdigit,i[3].replace(",",""))))
+                property_list.append(Property(i[0],i[1],price_2,row[4],None))
 print(i1,i2)
+for Property in property_list:
+    print(Property)
 
 
-
+with open('Right_move/items.json', 'w') as file:
+    json.dump([item.__dict__ for item in property_list], file)
