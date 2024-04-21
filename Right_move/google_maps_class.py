2d023a9e0ddd4e0fb3ac816d0900dd2df189bbb5
jontysh4w <79010302+jontysh4w@users.noreply.github.com>
2024-04-21 08:39:40 +0100

    Local database seperate from google sheets

--- Right_move/google_maps_class.py
+++ Right_move/google_maps_class.py
@@ -59,29 +59,14 @@ class house_hunter:
         self.journey_dict=person_destination
         self.link=open(LINK).readlines()
         self.time_dict={}
-
         #determines the web browser setup for Beautiful soup
         self.headers = {
             "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",  "Accept":"text/html,application/xhtml+xml,application/xml; q=0.9,image/webp,image/apng,*/*;q=0.8"
         }
 
-    def push_dict_to_shelf(link : str,dict : dict):
-        # file to be used
-        shelf = shelve.open("full_data.shlf")
-        # serializing
-        shelf[dict[link]]=dict
-        shelf.close() # you must close the shelve file!!!
-
-    def check_link_shelf(link : str):
-        shelf = shelve.open("full_data.shlf") # the same filename that you used before, please
-        try:
-            my_dict = shelf[link]
-        except:
-            print("No dictionary present")
-        shelf.close()
-
-
-    def sheet_hunter():
+    
+    def sheet_hunter(self):
+        print("sheet_hunting")
         """Returns a list containing: a dictionary of headers and their positions in the google sheets, a list of links taken from the document"""
         scopes= [
             "https://www.googleapis.com/auth/spreadsheets"
@@ -98,11 +83,16 @@ class house_hunter:
         headings_dict={}
         for i,j in enumerate(headings):
             headings_dict[str(j)]=i+1
+        #dictionary of headings
+        self.google_sheets_heading=headings_dict
         links=sheet.col_values(headings_dict["Link"])[1:]
+
+        #list of links
+        self.google_links=links
 
         with open("full_data.txt","rb") as f:
             full_data = f.read().decode("UTF-8")
-        print(full_data[0])
+        #print(full_data)
 
 
         return [headings_dict,links]
@@ -136,68 +126,69 @@ class house_hunter:
                     data_list.append(row)
 
 
-    def read(self, gmaps_true : bool, transport_mode : str, list_of_links : list):
-        def single_read(self, gmaps_true,transport_mode):
-            '''BOOL - using google maps, transport mode : transit, driving, walking, cycling '''
-            res = requests.get(self.link[0], headers=self.headers)
-            res.raise_for_status()
-            soup = BeautifulSoup(res.text, "html.parser")
+    def single_read(self, gmaps_true,transport_mode):
+        '''BOOL - using google maps, transport mode : transit, driving, walking, cycling '''
+        res = requests.get(self.link[0], headers=self.headers)
+        self.output_dict["Link"]=self.link[0]
+        res.raise_for_status()
+        soup = BeautifulSoup(res.text, "html.parser")
+        just_text=soup.get_text()
+        matches = [(match.group(), match.start()) for match in re.finditer(r'£\d{1,3},\d{3}', just_text)]
+        Deposit = None
+        Price = None
+        location = [(match.group(), match.start()) for match in re.finditer("for rent in", just_text)]
+        location_text=just_text[location[0][1]+11:location[0][1]+120].strip()
+        for i in matches:
+            index=i[1]
+            min_range = index-30
+            max_range = index+30
+            search_text=just_text[min_range:max_range]
+            if "Deposit" in search_text:
+                Deposit=i[0]
+            elif "Price" or "pcm" in search_text:
+                Price=i[0]
+        self.output_dict["Deposit"]=Deposit
+        self.output_dict["Price"]=Price
+        self.output_dict["Location"]=location_text
+        #alternate method for extracting location 
+        loc = soup.find(string=re.compile("for rent in"))
+        if gmaps_true:
+            my_key= os.getenv('GOOGLE_API_KEY')
+            gmaps = googlemaps.Client(key=my_key)
+            todayDate = datetime.today()
+            nextMonday = todayDate + timedelta(days=-todayDate.weekday(), weeks=1)
+            nextMonday = nextMonday.replace(hour=9,minute=0,second=0,microsecond=0)
+            geo_loc = gmaps.geocode(address=location_text)
+            geo_loc_lat_long= geo_loc[0]['geometry']['location']
+            nearest_station=gmaps.places_nearby(location=geo_loc_lat_long, radius=600,keyword="tube station")
+            stations_list=nearest_station['results']
+            stat_list=[]
+            tube_list=open('tube_stops.txt').read().splitlines()
+            for i in stations_list:
+                if any(word in i['name'] for word in tube_list):
+                    stat_list.append((i['name'],i['geometry']['location']))
+            dist_stat= gmaps.distance_matrix(location_text,stat_list[0][1],mode="walking",arrival_time=nextMonday)
+            #print(stat_list[0][0],dist_stat["rows"][0]["elements"][0]["duration"]["text"] )
+            self.output_dict["Local Station"]=stat_list[0][0]
+            for key in self.journey_dict:
+                dist= gmaps.distance_matrix(location_text,self.journey_dict[key],mode=transport_mode,arrival_time=nextMonday)
+                duration=dist["rows"][0]["elements"][0]["duration"]["text"] 
+                self.time_dict[key]=duration
+                self.output_dict["Work - "+key]=duration
+        else:
+            for key in self.journey_dict:
+                self.time_dict[key]="Empty"
 
-            just_text=soup.get_text()
-            matches = [(match.group(), match.start()) for match in re.finditer(r'£\d{1,3},\d{3}', just_text)]
-            Deposit = None
-            Price = None
-            location = [(match.group(), match.start()) for match in re.finditer("for rent in", just_text)]
+        #with shelve.open('full_data') as shelf:
+        #    shelf[self.link[0]] = self.output_dict
+        #    print("Data stored successfully.")
 
-            location_text=just_text[location[0][1]+11:location[0][1]+120].strip()
-            for i in matches:
-                index=i[1]
-                min_range = index-30
-                max_range = index+30
-                search_text=just_text[min_range:max_range]
-                if "Deposit" in search_text:
-                    Deposit=i[0]
-                elif "Price" or "pcm" in search_text:
-                    Price=i[0]
-            self.output_dict["Deposit"]=Deposit
-            self.output_dict["Price"]=Price
-            self.output_dict["Location"]=location_text
+        with shelve.open('full_data', 'r') as shelf:
+            retrieved_data = shelf[self.link[0]]
+            print("Data retrieved successfully:")
+            print(retrieved_data)
 
-            #alternate method for extracting location 
-            loc = soup.find(string=re.compile("for rent in"))
-
-
-            if gmaps_true:
-                my_key= os.getenv('GOOGLE_API_KEY')
-                gmaps = googlemaps.Client(key=my_key)
-                todayDate = datetime.today()
-                nextMonday = todayDate + timedelta(days=-todayDate.weekday(), weeks=1)
-                nextMonday = nextMonday.replace(hour=9,minute=0,second=0,microsecond=0)
-                geo_loc = gmaps.geocode(address=location_text)
-                geo_loc_lat_long= geo_loc[0]['geometry']['location']
-                nearest_station=gmaps.places_nearby(location=geo_loc_lat_long, radius=600,keyword="tube station")
-                stations_list=nearest_station['results']
-                stat_list=[]
-                tube_list=open('tube_stops.txt').read().splitlines()
-                for i in stations_list:
-                    if any(word in i['name'] for word in tube_list):
-                        stat_list.append((i['name'],i['geometry']['location']))
-                dist_stat= gmaps.distance_matrix(location_text,stat_list[0][1],mode="walking",arrival_time=nextMonday)
-
-                #print(stat_list[0][0],dist_stat["rows"][0]["elements"][0]["duration"]["text"] )
-                self.output_dict["Local Station"]=stat_list[0][0]
-
-                for key in self.journey_dict:
-                    dist= gmaps.distance_matrix(location_text,self.journey_dict[key],mode=transport_mode,arrival_time=nextMonday)
-                    duration=dist["rows"][0]["elements"][0]["duration"]["text"] 
-                    self.time_dict[key]=duration
-                    self.output_dict["Work - "+key]=duration
-            else:
-                for key in self.journey_dict:
-                    self.time_dict[key]="Empty"
-            
-
-        single_read(self,gmaps_true,transport_mode)
+        
         return self.time_dict
         
     
@@ -211,4 +202,5 @@ class house_hunter:
         print(self.output_dict)
 
 
-
+x=house_hunter({'Jonty':'Kings Langley','Taka':'Westminster','Jovan':'1 New St Square, London EC4A 3HQ '},'./link.txt')
+x.single_read(False,"transit")
