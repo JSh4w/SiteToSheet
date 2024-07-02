0b5dda898f876ca874886317c1a6b22a2fa7b8de
jontysh4w <79010302+jontysh4w@users.noreply.github.com>
2024-07-02 19:29:50 +0100

    Edits to cache to remove untracked directories and files from repo

--- Right_move/test/reading_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import re 
-data= pd.read_csv("Right_move/sales_data.csv")
-pattern = r"[A-Z]{1,2}\d[\dA-Z]? ?[\dA-Z]{0,3}"
-[print(str(i).split()[-1]) for i in data['Address'] if re.search(pattern, str(i).split()[-1])]
-
-
-    
